d6aeffe89b043c0f35ee70399dfe191907dee8ed
Hannan Fayyaz <hannanfayyaz@gmail.com>
2022-12-04 23:57:50 -0500

    updated

--- app.py
+++ app.py
@@ -6,22 +6,25 @@ import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
+cols = ["age" , "average_income" , "bmi",  "children", 'smoker_yes']
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
+
 def predict():
     feature_list = request.form.to_dict()
     feature_list = list(feature_list.values())
     feature_list = list(map(int, feature_list))
-    final_features = np.array(feature_list).reshape(1, 4) 
+    final_features = np.array(feature_list).reshape(1, 5) 
     
     prediction = model.predict(final_features)
     output = int(prediction[0])
    
-    return render_template('index.html', prediction_text='Life insurance Premium is:  {}'.format(text))
+    return render_template('index.html', prediction_text='Life insurance Premium is:  {}'.format(output))
 
 
 if __name__ == "__main__":
